7f26f2ecbb90e7f27fd7abd49157dcc07c22535e
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-02-22 23:41:06 +0600

    fixed JS part 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let interviewList = [];
+console.log(interviewList);
 let rejectedList = [];
 let allCount = document.getElementById('all-count');
 let interviewCount = document.getElementById('interview-count');
@@ -7,11 +8,13 @@ let rejectedCount = document.getElementById('rejected-count');
 let allButton = document.getElementById('all-btn');
 let interviewButton = document.getElementById('interview-btn');
 let rejectedButton = document.getElementById('rejected-btn');
+let count = document.getElementById('count');
 
 const allSection = document.getElementById('all-section');
 
-const allSectionContainer = document.querySelector('section');
-const interviewSection = document.getElementById('interview-section')
+let allSectionContainer = document.querySelector('section');
+const filterSection = document.getElementById('filtered-section')
+
 
 function countAll() {
     allCount.innerText = allSection.children.length; //8
@@ -20,6 +23,18 @@ function countAll() {
 }
 
 countAll();
+
+allButton.addEventListener('click', function () {
+    count.innerText = allSection.children.length;
+});
+
+interviewButton.addEventListener('click', function () {
+    count.innerText = filterSection.children.length;
+});
+
+rejectedButton.addEventListener('click', function () {
+    count.innerText = filterSection.children.length;
+});
 
 function toggleStyle(id) {
 
@@ -41,6 +56,14 @@ function toggleStyle(id) {
     // Adding black Bg for current button
     selected.classList.remove('btn-color');
     selected.classList.add('add-btn');
+
+    if (id === "interview-btn") {
+        allSection.style.display = "none";
+        filterSection.style.display = "block";
+    } else if (id === "all-btn") {
+        allSection.style.display = "block";
+        filterSection.style.display = "none";
+    }
 }
 
 allSectionContainer.addEventListener('click', function (event) {
@@ -60,29 +83,68 @@ allSectionContainer.addEventListener('click', function (event) {
             postName,
             right,
             title,
-            btnApplied,
+            btnApplied: "Interview",
             footerContentInfo,
             interview,
             rejected
         }
 
         const jobExist = interviewList.find(item => item.jobName === cardInfo.jobName);
-
+        parentNode.querySelector('.btn-applied').innerText = "Interview";
+        parentNode.querySelector('.btn-applied').style.border = "2px solid #10B981";
+        parentNode.querySelector('.btn-applied').style.color = "#10B981";
         if (!jobExist) {
             interviewList.push(cardInfo);
         }
 
+        countAll();
+
         renderInterview();
-    }
+    } // else if (event.target.classList.contains("rejected")) {
+    //     const parentNode = event.target.parentNode.parentNode;
+    //     const jobName = parentNode.querySelector('.job-name').innerText;
+    //     const postName = parentNode.querySelector('.post-name').innerText;
+    //     const right = parentNode.querySelector('.right').innerText;
+    //     const title = parentNode.querySelector('.title').innerText;
+    //     const btnApplied = parentNode.querySelector('.btn-applied').innerText;
+    //     const footerContentInfo = parentNode.querySelector('.footer-content-info').innerText;
+    //     const interview = parentNode.querySelector('.footer-content-info').innerText;
+    //     const rejected = parentNode.querySelector('.rejected').innerText;
+    //     const cardInfo = {
+    //         jobName,
+    //         postName,
+    //         right,
+    //         title,
+    //         btnApplied: "Interview",
+    //         footerContentInfo,
+    //         interview,
+    //         rejected
+    //     }
+
+    //     const jobExist = interviewList.find(item => item.jobName === cardInfo.jobName);
+    //     parentNode.querySelector('.btn-applied').innerText = "Interview";
+    //     parentNode.querySelector('.btn-applied').style.border = "2px solid #10B981";
+    //     parentNode.querySelector('.btn-applied').style.color = "#10B981";
+    //     /**
+    //      * border: 2px solid #10B981;
+    // color: #10B981;
+    //      */
+    //     if (!jobExist) {
+    //         interviewList.push(cardInfo);
+    //     }
+
+    //     countAll();
+
+    //     renderInterview();
+    // }
 });
 
 function renderInterview() {
-    interviewSection.innerHTML = "";
+    filterSection.innerHTML = "";
 
     for (let interview of interviewList) {
         console.log(interview);
         let div = document.createElement('div');
-        div.style.color = 'white';
         div.style.padding = '24px';
         div.style.border = '1px solid #F1F2F4';
         div.style.borderRadius = '8px';
@@ -90,7 +152,7 @@ function renderInterview() {
         div.innerHTML = `
         <div class="header-content">
                     <div class="left">
-                        <h3 class="job-name">Mobile First Corp</h3>
+                        <h3 class="job-name">${interview.jobName}</h3>
                         <p class="post-name">React Native Developer</p>
                     </div>
                     <div class="right">
@@ -101,7 +163,7 @@ function renderInterview() {
                 <p class="title">Remote • Full-time • $130,000 - $175,000</p>
 
                 <div class="footer-content">
-                    <button class="btn-applied">Not Applied</button>
+                    <button class="btn-applied">${interview.btnApplied}</button>
                     <p class="footer-content-info">Build cross-platform mobile applications using React Native. Work on products used by millions of
                         users worldwide.</p>
                 </div>
@@ -111,13 +173,6 @@ function renderInterview() {
                     <button class="rejected">REJECTED</button>
                 </div>
         `
-
-        /**
-         * background: white;
-    padding: 24px;
-    border: 1px solid #F1F2F4;
-    border-radius: 8px;
-    margin-bottom: 16px;
-         */
+        filterSection.appendChild(div);
     }
 }
