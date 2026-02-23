10919534ddd4426e30831810e8956f475a7aa33a
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-02-23 10:14:35 +0600

    fixed JS part 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let interviewList = [];
-console.log(interviewList);
 let rejectedList = [];
+let currentStatus = 'all';
 let allCount = document.getElementById('all-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
@@ -52,6 +52,8 @@ function toggleStyle(id) {
     // console.log(id);
 
     const selected = document.getElementById(id);
+    currentStatus = id;
+
 
     // Adding black Bg for current button
     selected.classList.remove('btn-color');
@@ -60,14 +62,20 @@ function toggleStyle(id) {
     if (id === "interview-btn") {
         allSection.style.display = "none";
         filterSection.style.display = "block";
+        renderInterview();
     } else if (id === "all-btn") {
         allSection.style.display = "block";
         filterSection.style.display = "none";
+    } else if (id === "rejected-btn") {
+        allSection.style.display = "none";
+        filterSection.style.display = "block";
+        renderRejected();
     }
 }
 
 allSectionContainer.addEventListener('click', function (event) {
     console.log(event.target.classList.contains("interview"));
+    console.log(event.target.classList.contains("right"));
     if (event.target.classList.contains("interview")) {
         const parentNode = event.target.parentNode.parentNode;
         const jobName = parentNode.querySelector('.job-name').innerText;
@@ -97,46 +105,51 @@ allSectionContainer.addEventListener('click', function (event) {
             interviewList.push(cardInfo);
         }
 
+        rejectedList = rejectedList.filter(item => item.jobName != cardInfo.jobName);
+
+        if (currentStatus === "rejected-btn") {
+            renderRejected();
+        }
+
         countAll();
 
-        renderInterview();
-    } // else if (event.target.classList.contains("rejected")) {
-    //     const parentNode = event.target.parentNode.parentNode;
-    //     const jobName = parentNode.querySelector('.job-name').innerText;
-    //     const postName = parentNode.querySelector('.post-name').innerText;
-    //     const right = parentNode.querySelector('.right').innerText;
-    //     const title = parentNode.querySelector('.title').innerText;
-    //     const btnApplied = parentNode.querySelector('.btn-applied').innerText;
-    //     const footerContentInfo = parentNode.querySelector('.footer-content-info').innerText;
-    //     const interview = parentNode.querySelector('.footer-content-info').innerText;
-    //     const rejected = parentNode.querySelector('.rejected').innerText;
-    //     const cardInfo = {
-    //         jobName,
-    //         postName,
-    //         right,
-    //         title,
-    //         btnApplied: "Interview",
-    //         footerContentInfo,
-    //         interview,
-    //         rejected
-    //     }
+    } else if (event.target.classList.contains("rejected")) {
+        const parentNode = event.target.parentNode.parentNode;
+        const jobName = parentNode.querySelector('.job-name').innerText;
+        const postName = parentNode.querySelector('.post-name').innerText;
+        const right = parentNode.querySelector('.right').innerText;
+        const title = parentNode.querySelector('.title').innerText;
+        const btnApplied = parentNode.querySelector('.btn-applied').innerText;
+        const footerContentInfo = parentNode.querySelector('.footer-content-info').innerText;
+        const interview = parentNode.querySelector('.footer-content-info').innerText;
+        const rejected = parentNode.querySelector('.rejected').innerText;
+        const cardInfo = {
+            jobName,
+            postName,
+            right,
+            title,
+            btnApplied: "Rejected",
+            footerContentInfo,
+            interview,
+            rejected
+        }
 
-    //     const jobExist = interviewList.find(item => item.jobName === cardInfo.jobName);
-    //     parentNode.querySelector('.btn-applied').innerText = "Interview";
-    //     parentNode.querySelector('.btn-applied').style.border = "2px solid #10B981";
-    //     parentNode.querySelector('.btn-applied').style.color = "#10B981";
-    //     /**
-    //      * border: 2px solid #10B981;
-    // color: #10B981;
-    //      */
-    //     if (!jobExist) {
-    //         interviewList.push(cardInfo);
-    //     }
+        const jobExist = rejectedList.find(item => item.jobName === cardInfo.jobName);
+        parentNode.querySelector('.btn-applied').innerText = "Rejected";
+        parentNode.querySelector('.btn-applied').style.border = "2px solid #ff0000cc";
+        parentNode.querySelector('.btn-applied').style.color = "#b91010c9";
 
-    //     countAll();
+        if (!jobExist) {
+            rejectedList.push(cardInfo);
+        }
 
-    //     renderInterview();
-    // }
+        interviewList = interviewList.filter(item => item.jobName != cardInfo.jobName);
+        if (currentStatus === "interview-btn") {
+            renderInterview();
+        }
+
+        countAll();
+    }
 });
 
 function renderInterview() {
@@ -173,6 +186,43 @@ function renderInterview() {
                     <button class="rejected">REJECTED</button>
                 </div>
         `
+        filterSection.appendChild(div);
+    }
+}
+function renderRejected() {
+    filterSection.innerHTML = "";
+
+    for (let rejected of rejectedList) {
+        console.log(rejected);
+        let div = document.createElement('div');
+        div.style.padding = '24px';
+        div.style.border = '1px solid #F1F2F4';
+        div.style.borderRadius = '8px';
+        div.style.marginBottom = '16px';
+        div.innerHTML = `
+        <div class="header-content">
+                    <div class="left">
+                        <h3 class="job-name">${rejected.jobName}</h3>
+                        <p class="post-name">React Native Developer</p>
+                    </div>
+                    <div class="right">
+                        <i class="fa-regular fa-trash-can delete-button"></i>
+                    </div>
+                </div>
+
+                <p class="title">Remote • Full-time • $130,000 - $175,000</p>
+
+                <div class="footer-content">
+                    <button class="btn-applied">${rejected.btnApplied}</button>
+                    <p class="footer-content-info">Build cross-platform mobile applications using React Native. Work on products used by millions of
+                        users worldwide.</p>
+                </div>
+
+                <div class="footer-btn">
+                    <button class="interview">INTERVIEW</button>
+                    <button class="rejected">REJECTED</button>
+                </div>
+        `;
         filterSection.appendChild(div);
     }
 }
